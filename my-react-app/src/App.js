2be3568dd06ad7a719247dd677bc5906e3cbae5f
Akari Hirohara <akari.hirohara@devplus.edu.vn>
2025-08-13 14:21:06 +0700

    t com

---
+++ my-react-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <title>crazy</title>
+        <h1 class="a">Rio and Takeru and Akari</h1>
+        <h2 class="b">we are crazy</h2>
+        <p>They are internship member!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
